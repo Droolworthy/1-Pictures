8dc9d7a001a5081a30c465b79d9351fd263529e9
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-06-13 15:30:27 +0300

    Добавьте файлы проекта.

---
+++ CS4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CS4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int picturesRow = 3;
+            int picturesАlbum = 52;
+            int filledRow = 52/3;
+            int beyondMeasure = 52;
+            Console.WriteLine(filledRow);
+            Console.WriteLine(beyondMeasure);
+        }
+    }
+}
